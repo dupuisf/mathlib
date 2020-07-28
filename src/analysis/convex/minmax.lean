39265076ce9539efead84ebe978dc2e5d59b6622
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2020-07-28 00:14:38 -0400

    clean up the code

--- src/analysis/convex/minmax.lean
+++ src/analysis/convex/minmax.lean
@@ -4,8 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Frédéric Dupuis
 -/
 
-import analysis.convex.basic
-import analysis.convex.topology
 import analysis.calculus.local_extr
 
 /-!
@@ -17,16 +15,15 @@ a global minimum.
 
 universes u
 
-variables {E : Type u} [normed_group E] [normed_space ℝ E]
-  {s : set E}
+variables {E : Type u} [normed_group E] [normed_space ℝ E] {s : set E}
 
-open set linear_map filter
+open set filter
 open_locale classical
 
-/-
+/--
 Helper lemma for the more general case: is_min_on_of_is_local_min_on_of_convex_on
 -/
-lemma is_min_on_of_is_local_min_on_of_convex_on_Icc {f : ℝ → ℝ} {a b : ℝ}
+lemma is_min_on.of_is_local_min_on_of_convex_on_Icc {f : ℝ → ℝ} {a b : ℝ}
   (a_lt_b : a < b) (h_local_min : is_local_min_on f (Icc a b) a) (h_conv : convex_on (Icc a b) f) :
   ∀ x ∈ Icc a b, f a ≤ f x :=
 begin
@@ -76,10 +73,10 @@ begin
     linarith [lt_on_nhd z h₁, ge_on_nhd z h₂] }
 end
 
-/-
+/--
 A local minimum of a convex function is a global minimum, restricted to a set s
 -/
-lemma is_min_on_of_is_local_min_on_of_convex_on {f : E → ℝ} {a : E}
+lemma is_min_on.of_is_local_min_on_of_convex_on {f : E → ℝ} {a : E}
   (a_in_s : a ∈ s) (h_localmin: is_local_min_on f s a) (h_conv : convex_on s f) :
   ∀ x ∈ s, f a ≤ f x :=
 begin
@@ -89,18 +86,14 @@ begin
 
   let g : affine_map ℝ ℝ ℝ E E := affine_map.line_map a (x - a),
 
-  have g_cont : continuous g := affine_map.line_map_continuous,
   have hg0 : g 0 = a := affine_map.line_map_apply_zero a (x - a),
   have hg1 : g 1 = x := by simp [affine_map.line_map_apply, one_smul],
 
   have fg_local_min_on : is_local_min_on (f ∘ g) (g ⁻¹' s) 0,
-  {
-    rw [←hg0] at h_localmin,
+  { rw [←hg0] at h_localmin,
     refine is_local_min_on.comp_continuous_on h_localmin subset.rfl
-      (continuous.continuous_on g_cont) _,
+      (continuous.continuous_on (affine_map.line_map_continuous)) _,
     simp [mem_preimage, hg0, a_in_s] },
-
-  have fg_conv : convex_on (g ⁻¹' s) (f ∘ g) := convex_on.affine_preimage h_conv,
 
   have fg_min_on : is_min_on (f ∘ g) (Icc 0 1) 0,
   { have Icc_in_s' : Icc 0 1 ⊆ (g ⁻¹' s),
@@ -108,41 +101,36 @@ begin
       have h1 : (1 : ℝ) ∈ (g ⁻¹' s) := by simp [mem_preimage, hg1, x_in_s],
 
       rw [←(segment_eq_Icc (show (0 : ℝ) ≤ 1, by linarith))],
-      exact convex.segment_subset (convex.affine_preimage h_conv.1)
+      exact convex.segment_subset (convex.affine_preimage g h_conv.1)
         (by simp [mem_preimage, hg0, a_in_s]) (by simp [mem_preimage, hg1, x_in_s]) },
 
     have fg_local_min_on' : is_local_min_on (f ∘ g) (Icc 0 1) 0 :=
       is_local_min_on.on_subset fg_local_min_on Icc_in_s',
 
-    refine is_min_on_of_is_local_min_on_of_convex_on_Icc (by linarith) fg_local_min_on' _,
-    exact convex_on.subset fg_conv Icc_in_s' (convex_Icc 0 1) },
+    refine is_min_on.of_is_local_min_on_of_convex_on_Icc (by linarith) fg_local_min_on' _,
+    exact convex_on.subset (convex_on.comp_affine_map g h_conv) Icc_in_s' (convex_Icc 0 1) },
 
-  have gx_lt_ga := calc
-                      (f ∘ g) 1 = f x           : by simp [hg1]
-                            ... < f a           : fx_lt_fa
-                            ... = (f ∘ g) 0     : by simp [hg0],
+  have gx_lt_ga : (f ∘ g) 1 < (f ∘ g) 0 := by simp [hg1, fx_lt_fa, hg0],
 
   rw [is_min_on_iff] at fg_min_on,
   linarith[fg_min_on 1 (mem_Icc.mpr ⟨zero_le_one, le_refl 1⟩)],
 end
 
-/-
+/--
 A local minimum of a convex function is a global minimum, restricted to a set s
 -/
-lemma is_min_on_of_is_local_min_on_of_convex_on' {f : E → ℝ} {a : E}
+lemma is_min_on.of_is_local_min_on_of_convex_on' {f : E → ℝ} {a : E}
   (as : a ∈ s) (h_local_min : is_local_min_on f s a) (h_conv : convex_on s f) :
   is_min_on f s a :=
-begin
-  dsimp [is_min_on, is_min_filter],
-  rw[eventually_principal],
-  exact is_min_on_of_is_local_min_on_of_convex_on as h_local_min h_conv
-end
+is_min_on.of_is_local_min_on_of_convex_on as h_local_min h_conv
 
-/-
-A local minimum of a convex function is a global minimum
--/
-lemma is_min_of_is_local_min_of_convex {f : E → ℝ} {a : E}
-  (h_local_min : is_local_min f a) (h_conv : convex_on univ f) :
-  ∀ x, f a ≤ f x :=
-λ x, (is_min_on_of_is_local_min_on_of_convex_on (mem_univ a)
+/-- A local minimum of a convex function is a global minimum -/
+lemma is_min_on.of_is_local_min_of_convex_univ {f : E → ℝ} {a : E}
+  (h_local_min : is_local_min f a) (h_conv : convex_on univ f) : ∀ x, f a ≤ f x :=
+λ x, (is_min_on.of_is_local_min_on_of_convex_on (mem_univ a)
         (is_local_min.on h_local_min univ) h_conv) x (mem_univ x)
+
+/-- A local minimum of a convex function is a global minimum -/
+lemma is_min_on.of_is_local_min_of_convex_univ' {f : E → ℝ} {a : E}
+  (h_local_min : is_local_min f a) (h_conv : convex_on univ f) : is_min_on f univ a :=
+λ x z, is_min_on.of_is_local_min_of_convex_univ h_local_min h_conv x
